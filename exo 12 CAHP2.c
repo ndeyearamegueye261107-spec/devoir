3d8aa4aec80b7f7a1db08e31b3fd1f8d464d303c
ndeyearamegueye261107-spec <ndeyearamegueye261107@gmail.com>
2026-02-11 07:39:18 +0000

    ini

---
+++ exo 12 CAHP2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int annee;
+printf("saisisser une annee");
+scanf("%d",annee);
+if(annee%4=0){
+    printf("%d est une annee bisextile")
+}
+else{
+    printf("%d n est pas annee bisextile")
+}
---
+++ exo gpt chap2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+int note,nbr;
+printf("donne ta note : ");
+scanf("%d",&note);
+printf("donne ton nombre dabsence : ");
+scanf("%d",&nbr);
+if(note>=0 && 10<=note<12 && 0<nbr<=5){
+    printf("passable");
+}
+else if(12<=note<14 && 0<nbr<=4){
+    printf("bien");
+}
+else if(14<=note<16 && 0<nbr<=3){
+    printf("tres bien");
+}
+else if(note>=16 && note<=20 && 0<nbr<=2){
+    printf("excellent");
+}
+else{
+    printf("insuffisant");
+}
+
+}
